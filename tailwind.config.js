3bf5a0c3ef98cf08b1f4e2788e9ac91afc07a543
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2022-06-04 06:45:29 +0530

    💄 added custom font and color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: ["Pacifico", "sans-serif"],
+      },
+      colors: {
+        "regal-blue": "#243c5a",
+      },
+    },
   },
   plugins: [],
 };
